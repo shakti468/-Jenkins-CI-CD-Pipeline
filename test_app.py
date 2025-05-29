17229cf67a3e8ce959909b6bdc6abde9419c27a0
Shakti <shaktiranjanm827@gmail.com>
2025-05-30 02:44:30 +0530

    Initial Flask app with test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
+
 from app import app
 
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, CI/CD World!" in response.data
+    assert b"Hello, Flask CI/CD is running successfully on EC2 with Jenkins and Nginx!" in response.data
