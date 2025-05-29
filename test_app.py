1aa4aaf2967721d683876acf3187487164f33304
Shakti Ranjan Mohanty <109822979+shakti468@users.noreply.github.com>
2025-05-30 01:32:19 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,7 @@
-# test_app.py
+from app.main import app
 
-import unittest
-from app import app
-
-class FlaskAppTests(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_home_status_code(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, CI/CD" in response.data
