489ad60f7d70c46e419ab90dbae73880981709ce
Shakti Ranjan Mohanty <109822979+shakti468@users.noreply.github.com>
2025-05-30 01:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, CI/CD World!"
+def hello():
+    return "Hello, CI/CD with Jenkins & GitHub Actions!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
